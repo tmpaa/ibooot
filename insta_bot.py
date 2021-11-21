f678b66ab4c860d31dce8b74ff9d9a6752033f9a
tmpaa <94754427+tmpaa@users.noreply.github.com>
2021-11-21 18:12:26 +0100

    Update insta_bot.py

--- insta_bot.py
+++ insta_bot.py
@@ -24,7 +24,7 @@ session.login()
 session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
 session.set_relationship_bounds(enabled=True, delimit_by_numbers=True, max_followers=10000)
 
-session.set_do_follow(True, percentage=70)
+#session.set_do_follow(True, percentage=70)
 session.set_do_comment(True, percentage=80)
 session.set_comments(["Nice!", "Awesome!", "Beautiful :heart_eyes:", "Wow!"])
 
